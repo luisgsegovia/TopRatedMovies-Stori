d17377130bc76ccde06f5e8557c706863413f530
Luis Segovia <luissegovia@outlook.com>
2024-07-27 17:55:00 -0600

    Add Movies service response model

---
+++ TopRatedMovies/Model/TopRatedMoviesServiceResponse.swift
@@ -0,0 +1,50 @@
+//
+//  TopRatedMoviesServiceResponse.swift
+//  TopRatedMovies
+//
+//  Created by Luis Segovia on 27/07/24.
+//
+
+// MARK: - TopRatedMoviesServiceResponse
+struct TopRatedMoviesServiceResponse: Codable {
+    let page: Int
+    let results: [MovieResult]
+    let totalPages, totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - MovieResult
+struct MovieResult: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let genreIds: [Int]
+    let id: Int
+    let originalLanguage, originalTitle, overview: String
+    let popularity: Double
+    let posterPath, releaseDate, title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIds = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+
