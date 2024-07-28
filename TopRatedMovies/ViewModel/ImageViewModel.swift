73a1bd3c2d68459afafad5eaefce9561268a8544
Luis Segovia <luissegovia@outlook.com>
2024-07-27 23:46:41 -0600

    Create image view model for image handling

---
+++ TopRatedMovies/ViewModel/ImageViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  ImageViewModel.swift
+//  TopRatedMovies
+//
+//  Created by Luis Segovia on 27/07/24.
+//
+
+import Foundation
+
+final class ImageViewModel {
+    private let imageLoader: MovieImageLoaderProtocol
+    private var task: Task<Data, Error>?
+
+    var state: State = .loading
+
+    init(imageLoader: any MovieImageLoaderProtocol) {
+        self.imageLoader = imageLoader
+    }
+
+    func retrieveImage(from path: String) {
+        Task {
+            let task = await imageLoader.retrieveImage(from: path)
+            self.task = task
+            let result = await task.result
+
+            switch result {
+            case .success(let data):
+                state = .idle(data: data)
+            case .failure(let failure):
+                state = .placeholder
+            }
+        }
+    }
+
+    func cancel() {
+        guard let task = self.task,
+        !task.isCancelled else { return }
+
+        task.cancel()
+    }
+}
+
+extension ImageViewModel {
+    enum State: Equatable {
+        case loading
+        case idle(data: Data)
+        case placeholder
+    }
+}
