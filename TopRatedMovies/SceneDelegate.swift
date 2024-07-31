4dec7135f4e6c8b72f0577f967311ff9b44fea96
Luis Segovia <luissegovia@outlook.com>
2024-07-30 21:02:39 -0600

    Improve composition separation on dependency injection for better readability

--- TopRatedMovies/SceneDelegate.swift
+++ TopRatedMovies/SceneDelegate.swift
@@ -18,7 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let viewModel = TopRatedMoviesViewModel(moviesLoader: TopRatedMoviesLoader(client: URLSessionHTTPClient.init(session: .shared)))
+        let client = URLSessionHTTPClient(session: .shared)
+        let moviesLoader = TopRatedMoviesLoader(client: client)
+        let viewModel = TopRatedMoviesViewModel(moviesLoader: moviesLoader)
         let viewController = TopRatedMoviesListViewController(viewModel: viewModel)
         let navigation = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigation
